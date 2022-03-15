9604ad7fa44c98a8e1baadeb3df5b4717bf924c2
Yusuf Indra Bayu <68705079+NeFoG28@users.noreply.github.com>
2022-03-16 02:53:43 +0700

    Add files via upload

---
+++ LatihanStream_1180/LatihanStream_1180/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace LatihanStream_1180
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.btnLast);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.btnPrev);
+            this.panel1.Controls.Add(this.btnFirst);
+            this.panel1.Controls.Add(this.txtAlamat);
+            this.panel1.Controls.Add(this.txtNama);
+            this.panel1.Controls.Add(this.txtId);
+            this.panel1.Controls.Add(this.lblTotal);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(20, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 155);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id Customer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Alamat";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(230, 18);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(69, 13);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "Total Record";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(87, 15);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 4;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(87, 44);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(157, 20);
+            this.txtNama.TabIndex = 5;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(87, 75);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(201, 20);
+            this.txtAlamat.TabIndex = 6;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(31, 117);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(48, 24);
+            this.btnFirst.TabIndex = 7;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(85, 117);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(48, 24);
+            this.btnPrev.TabIndex = 8;
+            this.btnPrev.Text = "Prev";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(139, 117);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(48, 24);
+            this.btnNext.TabIndex = 9;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(193, 117);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(48, 24);
+            this.btnLast.TabIndex = 10;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(247, 117);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(48, 24);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(301, 117);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(48, 24);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(192, 195);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(71, 24);
+            this.btnOpen.TabIndex = 13;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(269, 195);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(71, 24);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(346, 195);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(71, 24);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 231);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Customer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+    }
+}
+
---
+++ LatihanStream_1180/LatihanStream_1180/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LatihanStream_1180
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            aktikanTextbox(false);
+            totalRecord();
+
+        }
+
+        const int kapasitasAwal = 50;
+        string[] arrCustomer = new string[kapasitasAwal];
+        int jmlMax = kapasitasAwal;
+        int idx = -1;
+        int jmlCustomer = 0;
+        char pemisah = ',';
+
+        private void pisahDataCustomer(string customer)
+        {
+            char[] pisah = { pemisah };
+            string[] dataCustomer = customer.Split(pisah);
+            txtId.Text = dataCustomer[0];
+            txtNama.Text = dataCustomer[1];
+            txtAlamat.Text = dataCustomer[2];
+
+        }
+
+        private void aktikanTextbox(bool sifatKeaktifan)
+        {
+            txtId.Enabled = sifatKeaktifan;
+            txtNama.Enabled = sifatKeaktifan;
+            txtAlamat.Enabled = sifatKeaktifan;
+        }
+
+        private void bersih()
+        {
+            txtId.Clear();
+            txtNama.Clear();
+            txtAlamat.Clear();
+
+        }
+
+        private void totalRecord()
+        {
+            lblTotal.Text = "Total Record = " + jmlCustomer.ToString();
+        }
+
+        private void updateDataArray()
+        {
+            if (jmlCustomer > 0)
+            {
+                //masing-masing field dipisahkan dengan tanda pemisah koma
+                string customer = "";
+                customer = customer + txtId.Text + pemisah;
+                customer = customer + txtNama.Text + pemisah;
+                customer = customer + txtAlamat.Text + pemisah;
+
+                //simpan string ke array
+                arrCustomer[idx] = customer;
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            DialogResult pilih = dlgOpen.ShowDialog();
+            if(pilih == DialogResult.OK)
+            {
+                arrCustomer = File.ReadAllLines(dlgOpen.FileName);
+                jmlCustomer = arrCustomer.Length;
+                idx = 0;
+
+                //perbesar ukuran array agar dapat ditambah record
+                jmlMax = jmlCustomer * 2;
+                Array.Resize(ref arrCustomer, jmlMax);
+
+                //tampilkan data ke textbox
+                string customer = arrCustomer[idx];
+                pisahDataCustomer(customer);
+                aktikanTextbox(true);
+                totalRecord();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            updateDataArray();
+            DialogResult pilih = dlgSave.ShowDialog();
+            if (pilih == DialogResult.OK)
+            {
+                //simpan data sebanyak jml data yang ada dlm array
+                //dengan cara copy data array lama ke array baru
+                string[] arrBantuan = new string[jmlCustomer];
+                Array.Copy(arrCustomer, arrBantuan, jmlCustomer);
+                File.WriteAllLines(dlgSave.FileName, arrBantuan);
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            updateDataArray();
+            if(jmlCustomer>0)
+            {
+                idx = 0;
+                string customer = arrCustomer[idx];
+                pisahDataCustomer(customer);
+                totalRecord();
+
+            }
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            updateDataArray();
+            if (jmlCustomer > 0)
+            {
+                idx = jmlCustomer - 1;
+                string customer = arrCustomer[idx];
+                pisahDataCustomer(customer);
+                totalRecord();
+
+            }
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            updateDataArray();
+            if (jmlCustomer > 0)
+            {
+                idx --;
+                if (idx < 0)
+                    idx = 0;
+
+                string customer = arrCustomer[idx];
+                pisahDataCustomer(customer);
+                totalRecord();
+
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            updateDataArray();
+            if (jmlCustomer > 0)
+            {
+                idx++;
+                if (idx >= jmlCustomer)
+                    idx = jmlCustomer - 1;
+
+                string customer = arrCustomer[idx];
+                pisahDataCustomer(customer);
+                totalRecord();
+
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            aktikanTextbox(true);
+            updateDataArray();
+            if (jmlCustomer == jmlMax)
+            {
+                jmlMax *= 2;
+                Array.Resize(ref arrCustomer, jmlMax);
+            }
+
+            bersih();
+            txtId.Focus();
+            idx = jmlCustomer;
+            jmlCustomer++;
+            totalRecord();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            updateDataArray();
+            if (jmlCustomer > 0)
+            {
+                if (idx == jmlCustomer - 1)
+                    idx--;
+                else
+                    for (int i = idx; i < jmlCustomer; i++)
+                        arrCustomer[i] = arrCustomer[i + 1];
+
+                jmlCustomer--;
+
+                if(jmlCustomer>0)
+                {
+                    string customer = arrCustomer[idx];
+                    pisahDataCustomer(customer);
+                }
+                else
+                {
+                    bersih();
+                    aktikanTextbox(false);
+                }
+
+                totalRecord();
+            }
+        }
+    }
+
+}
