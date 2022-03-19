dcde9381315d0fb3fcc77518af223c749e4481c5
Yusuf Indra Bayu <68705079+NeFoG28@users.noreply.github.com>
2022-03-20 06:53:52 +0700

    Game Race With Thread

---
+++ AplikasiGameThread_1180/AplikasiGameThread_1180/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace AplikasiGameThread_1180
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Player 1 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(316, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(407, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Player 2 :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(560, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Player 3 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(608, 291);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(771, 256);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(722, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(21, 256);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(170, 286);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 7;
+            this.btnPlay.Text = "Play Game";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox2.Location = new System.Drawing.Point(85, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(21, 256);
+            this.textBox2.TabIndex = 4;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::AplikasiGameThread_1180.Properties.Resources.motor31;
+            this.pictureBox3.Location = new System.Drawing.Point(6, 179);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(73, 56);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AplikasiGameThread_1180.Properties.Resources.motor21;
+            this.pictureBox2.Location = new System.Drawing.Point(6, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(73, 61);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AplikasiGameThread_1180.Properties.Resources.motor;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(816, 324);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Racing With Thread";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ AplikasiGameThread_1180/AplikasiGameThread_1180/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace AplikasiGameThread_1180
+{
+    public partial class Form1 : Form
+    {
+        public bool PlayAgain = false;
+        static Random r;
+        static Thread Player1Thd;
+        static Thread Player2Thd;
+        static Thread Player3Thd;
+        public static int countPlayer1;
+        public static int countPlayer2;
+        public static int countPlayer3;
+        public Form1()
+        {
+            InitializeComponent();
+            countPlayer1 = 0;
+            countPlayer2 = 0;
+            countPlayer3 = 0;
+            r = new Random();
+        }
+
+        public static void Player1()
+        {
+            while (true)
+            { Thread.Sleep(50);
+                if (countPlayer1 > 650)
+                {
+                    Player1Thd.Abort();
+                    break;
+                }
+                countPlayer1 = countPlayer1 + (1 + r.Next(6));
+            }
+        }
+
+        public static void Player2()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                if (countPlayer2 > 650)
+                {
+                    Player2Thd.Abort();
+                    break;
+                }
+                countPlayer2 = countPlayer2 + (1 + r.Next(6));
+            }
+        }
+
+        public static void Player3()
+        {
+            while (true)
+            {
+                Thread.Sleep(50);
+                if (countPlayer3 > 650)
+                {
+                    Player3Thd.Abort();
+                    break;
+                }
+                countPlayer3 = countPlayer3 + (1 + r.Next(6));
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            countPlayer1 = 0;
+            countPlayer2 = 0;
+            countPlayer3 = 0;
+
+            Player1Thd = new Thread(new ThreadStart(Player1));
+            Player1Thd.IsBackground = true;
+            Player1Thd.Start();
+
+            Player2Thd = new Thread(new ThreadStart(Player2));
+            Player2Thd.IsBackground = true;
+            Player2Thd.Start();
+
+            Player3Thd = new Thread(new ThreadStart(Player3));
+            Player3Thd.IsBackground = true;
+            Player3Thd.Start();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool check = false;
+            if (countPlayer1 >650)
+            {
+                timer1.Stop();
+                countPlayer1 = 0;
+                countPlayer2 = 0;
+                countPlayer3 = 0;
+                check = true;
+                int i = Int32.Parse(label2.Text);
+                i++;
+                label2.Text = i.ToString();
+                MessageBox.Show("Player one win the race");
+            }
+            if (countPlayer2 > 650)
+            {
+                timer1.Stop();
+                countPlayer1 = 0;
+                countPlayer2 = 0;
+                countPlayer3 = 0;
+                check = true;
+                int i = Int32.Parse(label4.Text);
+                i++;
+                label4.Text = i.ToString();
+                MessageBox.Show("Player two win the race");
+            }
+            
+            if (countPlayer3 > 650)
+            {
+                timer1.Stop();
+                countPlayer1 = 0;
+                countPlayer2 = 0;
+                countPlayer3 = 0;
+                check = true;
+                int i = Int32.Parse(label6.Text);
+                i++;
+                label6.Text = i.ToString();
+                MessageBox.Show("Player three win the race");
+            }
+
+            if (check == false)
+            {
+                pictureBox1.Left = countPlayer1;
+                pictureBox2.Left = countPlayer2;
+                pictureBox3.Left = countPlayer3;
+            }
+
+        }
+    }
+}
